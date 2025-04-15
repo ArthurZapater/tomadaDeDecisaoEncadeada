c6d45d19c090b040c722fae29459831321d04d24
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-14 22:42:32 -0300

    Subindo a classe Exemplo

---
+++ src/Exemplo.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Exemplo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double media;
+
+        System.out.print("Informe sua média em programação --> ");
+        media = sc.nextDouble();
+
+        if (media >= 6){
+            System.out.println("APROVADO");
+        }
+        else {
+            if (media< 4){
+                System.out.println("REPROVADO");
+            }
+            else {
+                System.out.println("EXAME");
+            }
+        }
+
+
+
+    }
+}
